529a84a654fe2cdba298cde49130f8e471c5bb2c
davidkmck <108087097+davidkmck@users.noreply.github.com>
2026-08-22 18:11:12 -0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'rome-2-c-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './landmarks.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
